9a54390307d61dc52a49c33282f2778c9e2094fc
robbert <robbert@vdzon.com>
2018-12-26 10:03:17 +0100

    share state via zookeeper

--- distributedstatemachine/readme.md
+++ distributedstatemachine/readme.md
@@ -1,3 +1,7 @@
+Sample statemachine applicatie, die z'n state opslaat in een remote zookeeper instantie.
+Hierdoor kunnen er meerdere instanties van deze applicatie draaien die de state delen. 
+
+
 # Initiele configuratie:
 oc new-build --name dstatemachine --image-stream="openshift/redhat-openjdk18-openshift:latest" --binary=true -n rzo-test 
 oc start-build dstatemachine --from-file=target\dstatemachine-0.0.1-SNAPSHOT.jar
@@ -8,3 +12,6 @@ oc create -f openshift-scripts\route.yml -n rzo-test
 # redeploy new version
 mvn install
 oc start-build dstatemachine --from-file=target\distributedstatemachine-0.0.1-SNAPSHOT.jar
+
+# you need to have a zookeeper running on localhost: 2181.  
+oc port-forward `oc get pod | grep rzozookeeper | grep -v build | awk '{print $1;}'` 2181:2181 
--- distributedstatemachine/src/main/java/com/example/distributedstatemachine/SimpleStateMachineConfiguration.java
+++ distributedstatemachine/src/main/java/com/example/distributedstatemachine/SimpleStateMachineConfiguration.java
@@ -1,20 +1,56 @@
 package com.example.distributedstatemachine;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
-import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.ensemble.StateMachineEnsemble;
+import org.springframework.statemachine.zookeeper.ZookeeperStateMachineEnsemble;
 
-import java.util.Arrays;
 import java.util.EnumSet;
-import java.util.HashSet;
 
 @Configuration
 @EnableStateMachine
+@Slf4j
 public class SimpleStateMachineConfiguration
         extends EnumStateMachineConfigurerAdapter<States, Events> {
+
+    @Value("${zookeeperhost}")
+    String zookeeperhost;
+
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<States, Events> states)
+            throws Exception {
+        states
+                .withDistributed()
+                .ensemble(stateMachineEnsemble())
+                .and()
+                .withConfiguration()
+                .autoStartup(true);
+    }
+
+    @Bean
+    public StateMachineEnsemble<States, Events> stateMachineEnsemble() throws Exception {
+        return new ZookeeperStateMachineEnsemble<States, Events>(curatorClient(), "/foo");
+    }
+
+    @Bean
+    public CuratorFramework curatorClient() throws Exception {
+        log.info("Remote zookeeper:" + zookeeperhost);
+        CuratorFramework client = CuratorFrameworkFactory.builder().defaultData(new byte[0])
+                .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                .connectString(zookeeperhost + ":2181").build();
+        client.start();
+        return client;
+    }
 
     @Override
     public void configure(StateMachineStateConfigurer<States, Events> states)
