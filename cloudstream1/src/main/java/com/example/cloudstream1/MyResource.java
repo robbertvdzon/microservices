3f5ab31463024f8e6e5bc6da1e294936b5fd7d91
robbert <robbert@vdzon.com>
2019-01-01 20:33:52 +0100

    async rest call: using MonoProcessor insted of CompletableFutures

--- cloudstream1/src/main/java/com/example/cloudstream1/MyResource.java
+++ cloudstream1/src/main/java/com/example/cloudstream1/MyResource.java
@@ -1,5 +1,6 @@
 package com.example.cloudstream1;
 
+import org.reactivestreams.Processor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -7,16 +8,16 @@ import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoProcessor;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @EnableBinding(QuestionStream.class)
 public class MyResource {
-    Map<String, CompletableFuture<String>> completableFutures = new HashMap<>();
+    Map<String, MonoProcessor<String>> completableFutures = new HashMap<>();
 
     @Autowired
     QuestionStream myStream;
@@ -24,21 +25,22 @@ public class MyResource {
     @GetMapping("/test")
     public Mono<String> test(){
         String uuid = UUID.randomUUID().toString();
-        Mono<String> defer = Mono.fromFuture(createFuture(uuid));
+
+        Mono<String> defer = Mono.from(createFuture(uuid));
         myStream.outboundQuestion().send(MessageBuilder.withPayload(new QuestionEvent(uuid, "My Question")).build());
         return defer;
     }
 
     @StreamListener(QuestionStream.INPUT)
     public void handle(AnswerEvent answer) {
-        completableFutures.get(answer.getQuestionId()).complete(answer.getAnswer());
+        completableFutures.get(answer.getQuestionId()).onNext(answer.getAnswer());
         System.out.println("Got answer: " + answer);
     }
 
-    private CompletableFuture<String> createFuture(String uuid) {
-        CompletableFuture<String> completableFuture = new CompletableFuture<>();
-        completableFutures.put(uuid, completableFuture);
-        return completableFuture;
+    private Processor<String, String> createFuture(String uuid) {
+        MonoProcessor<String> future = MonoProcessor.create();
+        completableFutures.put(uuid, future);
+        return future;
     }
 
 
